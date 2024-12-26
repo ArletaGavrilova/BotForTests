f8ef29d22ec23134f60d66eefc0c6bcdc8c632db
ArletaGavrilova <109897868+ArletaGavrilova@users.noreply.github.com>
2024-12-26 19:14:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,19 @@
 import telebot # библиотека telebot
 from config import token # импорт токена
+import telebot 
+from config import token
+from logic import Pokemon
 
 bot = telebot.TeleBot(token) 
+
+@bot.message_handler(commands=['go'])
+def go(message):
+    if message.from_user.username not in Pokemon.pokemons.keys():
+        pokemon = Pokemon(message.from_user.username)
+        bot.send_message(message.chat.id, pokemon.info())
+        bot.send_photo(message.chat.id, pokemon.show_img())
+    else:
+        bot.reply_to(message, "Ты уже создал себе покемона")
 
 @bot.message_handler(commands=['start'])
 def start(message):
